bb514888e0e6577c5b2165ff5c19117384113e49
Shyam101 <50941903+Shyam101@users.noreply.github.com>
2023-08-22 15:15:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const request = require('request')
+// parse urlencoded request bodies into req.body
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8080
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+  let body = ''
+
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+  })
+  
+  req.on('end', () => {
+    let payload = JSON.parse(body)
+    
+    if (payload.Type === 'SubscriptionConfirmation') {
+      const promise = new Promise((resolve, reject) => {
+        const url = payload.SubscribeURL
+        
+        request(url, (error, response) => {
+          if (!error && response.statusCode == 200) {
+            console.log('Yess! We have accepted the confirmation from AWS')
+            return resolve()
+          } else {
+            return reject()
+          }
+        })
+      })
+
+      promise.then(() => {
+        res.end("ok")
+      })
+    }
+  })
+})
+
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
